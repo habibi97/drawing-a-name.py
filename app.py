90321883bfe723cd3b0aef80ac243cd4f045ad0b
habibi97 <118036272+habibi97@users.noreply.github.com>
2022-11-12 07:55:29 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+print("|\             O              /|")
+print("| \             H            / | ")
+print("|  \             A          /  |")
+print("|   \             M        /   |")
+print("|    \             M      /    |")
+print("|     \            E     /     |")
+print("|      \          D     /      |")
+print("|       \        S     /       |")
+print("|        \      H     /        |")
+print("|         \    B     /         |")
+print("|          \  E     /          |")
+print("|           \E     /           |")
+print("|            \B   /            |")
+print("|             \  /             |")
+print("|    1   9     \/    9  7      |")
+
+
+
+
